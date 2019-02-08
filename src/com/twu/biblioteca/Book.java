3b5024b4450801440ac7fc221f31c132c45a287e
Luan Andrade <luan.andrade@thoughtworks.com>
2019-02-08 09:54:37 -0200

    1.2 view a list of all books

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+public class Book {
+
+    private String title;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 import java.io.*;
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,10 +12,20 @@ import static org.hamcrest.CoreMatchers.*;
 public class ExampleTest {
 
     private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private ArrayList<Book> books = new ArrayList<Book>();
 
     @Before
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
+    }
+
+    @Before
+    public void setBooks() {
+        for (int i=1; i<=7; i++) {
+            Book book = new Book();
+            book.setTitle("Game of thrones vol " + i);
+            books.add(book);
+        }
     }
 
     @Test
@@ -28,5 +39,26 @@ public class ExampleTest {
         biblioteca.main(null);
         String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great books titles in Bangalore!\n";
         assertThat(outContent.toString(), is(equalTo(welcomeMessage)));
+    }
+
+    @Test
+    public void testBook() {
+        Book book = new Book();
+        book.setTitle("Game of Thrones");
+        String title = "Game of Thrones";
+
+        assertThat(title, is(equalTo(book.getTitle())));
+    }
+
+    @Test
+    public void sizeOfBooksList() {
+        assertThat(7, is(equalTo(books.size())));
+    }
+
+    @Test
+    public void listAllBooks() {
+        for(int i=0; i<7; i++) {
+            assertThat("Game of thrones vol " + (i + 1), is(equalTo(books.get(i).getTitle())));
+        }
     }
 }
