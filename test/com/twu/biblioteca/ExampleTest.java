3993546fc899cc952796d73cb119b5c8fd3c78f5
Luan Andrade <luan.andrade@thoughtworks.com>
2019-02-08 09:00:46 -0200

    1.1 view a welcome message

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,32 @@
 package com.twu.biblioteca;
+import java.io.*;
 
-
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 
 public class ExampleTest {
+
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Test
     public void test() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void testWelcomeMessage() {
+        BibliotecaApp biblioteca = new BibliotecaApp();
+        biblioteca.main(null);
+        String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great books titles in Bangalore!\n";
+        assertThat(outContent.toString(), is(equalTo(welcomeMessage)));
     }
 }
